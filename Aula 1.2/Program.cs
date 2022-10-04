1d7f3edf05b0f92a95d0c00f0b125d08e20c45f1
Leoncio Neto <le.bdes.neto@gmail.com>
2022-10-04 16:55:27 +0100

    aula 1 Exercicio 2.3

--- Aula 1.2/Program.cs
+++ Aula 1.2/Program.cs
@@ -23,6 +23,7 @@ namespace Aula_1._2
             int total = numero * numero2 * numero3;
             Console.WriteLine("A multiplicaçao dos valores {0} {1} {2} é  {3} ", numero,numero2,numero3, total);*/
 
+            /* Exercicio 2.3*/
             Console.WriteLine("Digite um numero: ");
             int numero = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Digite um numero: ");
@@ -31,10 +32,13 @@ namespace Aula_1._2
             int subtracao = numero - numero2;
             int mult = numero * numero2;
             int div = numero/ numero2;
+            int MOD = numero % numero2;
             Console.WriteLine("A adiçao dos numeros {0} e {1} é = {2}",numero, numero2, adicao);
             Console.WriteLine("A subtração dos numeros {0} e {1} é = {2}", numero, numero2, subtracao);
             Console.WriteLine("A multiplicação dos numeros {0} e {1} é = {2}", numero, numero2, mult);
             Console.WriteLine("A divisão dos numeros {0} e {1} é = {2}", numero, numero2, div);
+            Console.WriteLine("O resto da divição dos numeros {0} e {1} é = {2}", numero, numero2, MOD);
+
         }
     }
 }
