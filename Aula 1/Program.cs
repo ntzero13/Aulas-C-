f7c5e8bd7cb9f3226363ac326df3c9934f375375
Leoncio Neto <le.bdes.neto@gmail.com>
2022-10-04 16:39:49 +0100

    Aula 1 exercicio 2

--- Aula 1/Program.cs
+++ Aula 1/Program.cs
@@ -21,22 +21,8 @@ namespace Aula_1
             numero = Console.ReadLine();
             Console.WriteLine(numero);*/
 
-            /* Exercicio 2.1
-            string numero1,numero2;
-            numero1 = Console.ReadLine();
-            numero2 = Console.ReadLine();
-            Console.WriteLine(numero2);
-            Console.WriteLine(numero1);*/
-
-            /* Exercicio 2.2*/
-            Console.WriteLine("Digite um numero: ");
-            int numero = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Digite um numero: ");
-            int numero2 = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Digite um numero: ");
-            int numero3 = Int32.Parse(Console.ReadLine());
-            int total = numero * numero2 * numero3;
-            Console.WriteLine("A multiplicaçao dos valores {0} {1} {2} é  {3} ", numero,numero2,numero3, total);
+           
+            
 
         }
     }
